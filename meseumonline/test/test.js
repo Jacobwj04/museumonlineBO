af35d9dcd6e85bb19f797c02b6a66302b72fd75d
leon kruijer <32770@ma-web.nl>
2022-01-30 21:13:30 +0100

    Leon en Jacob live share Tour

--- meseumonline/test/test.js
+++ meseumonline/test/test.js
@@ -1,44 +1,139 @@
-const mytitle = document.getElementById("mytitle");
-const myimage = document.getElementById("myimage");
-const myinput = document.getElementById("myinput");
+const myTitle = document.getElementById("myTitle");
+const myImage = document.getElementById("myImage");
+const myInput = document.getElementById("myInput");
+const myText = document.getElementById("myText");
 
-
-let current_index = 0;
-
-let lokaties =[ // array
-    { // 0 = object
-        "titel":"plaats 0",
-        "image":"imgtour/img1.jpeg",
-        "text":"de kamerwaar de nacht wacht in staat"
-    },
-    {  //1 = object 
-        "titel":"plaats 1",
-        "image":"imgtour/img2.jpg"
+let lokaties =
+[
+    {
+        "titel":"Het gebouw",
+        "image":"img/0.jpg",
+        "text":"Het werd in 1656 gebouwd als het ‘zeemagazijn’ van de Admiraliteit van Amsterdam, oftewel: een pakhuis vol scheepsmaterialen, maar in 1972 wordt het zeemagazijn een museum."
     },
     {
-        "titel":"plaats 2",
-        "image":"imgtour/img3.jpg"
+        "titel":"Open Plein: Zeemagazijn en overkapping",
+        "image":"img/1.jpg",
+        "text":"Tussen 2007 en 2011 heeft het museum een grootscheepse renovatie ondergaan. Daarbij werd de historische binnenplaats van een dak voorzien. Het ontwerp is geïnspireerd op de kompaslijnen van oude zeekaarten."
     },
     {
-        "titel":"plaats 3",
-        "image":"imgtour/img4.jpg"
+        "titel":"Republiek aan Zee",
+        "image":"img/2.jpg",
+        "text":"Deze tentoonstelling met de naam ‘Republiek aan Zee’ belicht de innige, historische band van Nederland met de zee. Het verhaal wordt verteld aan de hand van 50 bijzondere objecten, waaronder veel topstukken uit de museumcollectie. De objecten komen uit de 17e en 18e eeuw, de periode dat Nederland een Republiek was."
     },
     {
-        "titel":"plaats 4",
-        "image":"imgtour/img5.jpg"
+        "titel":"Willem van de Velde de Oude",
+        "image":"img/3.jpg",
+        "text":"Willem van de Velde de Oude werd in Leiden geboren als de zoon van een Vlaamse schipper, Willem Willemszoon van de Velde. Hij raakte al op jonge leeftijd door het werk van zijn vader bekend met de scheepvaart. Het is onbekend wanneer hij precies met schilderen begon, maar begin jaren dertig van de zeventiende eeuw startte hij een schildersatelier in Amsterdam."
+    },
+    {
+        "titel":"Zeeslag bij Gibraltar",
+        "image":"img/4.jpg",
+        "text":"De zeeslag bij Gibraltar tussen de Nederlandse en de Spaanse vloot, 25 april 1607, Cornelis Claesz van Wieringen (circa 1575 - 1633), olieverf op doek, 1622. [A.0724]"
+    },
+    {
+        "titel":"Braziliaans landschap, Frans Post",
+        "image":"img/5.jpg",
+        "text":"Dit schilderij van de Nederlandse schilder Frans Post toont een idyllisch Braziliaans landschap. Zo’n 8 jaar lang woonde en werkte Post in Brazilië in opdracht van de gouverneur-generaal van Nederlands-Brazilië, graaf Johan Maurits van Nassau-Siegen. Deze vroeg Post om het land zo nauwkeurig mogelijk vast te leggen."
+    },
+    {
+        "titel":"Republiek aan Zee: Zeeslag bij Gibraltar",
+        "image":"img/6.jpg",
+        "text":"De zeeslag bij Gibraltar tussen de Nederlandse en de Spaanse vloot, 25 april 1607, Cornelis Claesz van Wieringen (circa 1575 - 1633), olieverf op doek, 1622. [A.0724]"
+    },
+    {
+        "titel":"Ooggetuigen met Kadir van Lohuizen",
+        "image":"img/7.jpg",
+        "text":"De tekst over het volgende kunstwerk is geschreven door Kadir van Lohuizen."
+    },
+    {
+        "titel":"Republiek aan Zee: Dhr en mevr Sweers-Bloemaert",
+        "image":"img/8.jpg",
+        "text":"Deze twee portretten van het echtpaar Isaac Sweers en Constantia Bloemaert zijn een toonbeeld van de rijke burgerklasse in de Republiek. Isaac draagt een met goud- en zilverdraad gedecoreerde sjerp. Zijn linkerhand rust op zijn zwaard, in zijn rechterhand houdt hij een pistool. Constantia’s zijden jurk is met kostbaar kant afgezet en ze draagt parels aan haar oren, om haar hals en polsen. Een leuk detail: in de weerspiegeling van de ijzeren halsplaat van Isaac is een klein zelfportret van de schilder Luttichuijs te zien."
+    },
+    {
+        "titel":"Penschilderijen met Caroline van der Elst",
+        "image":"img/9.jpg",
+        "text":"De tekst over de aankomende paar kunstwerken is geschreven door Caroline van der Elst."
+    },
+    {
+        "titel":"Republiek aan Zee: Rede van Batavia",
+        "image":"img/10.jpg",
+        "text":"Dit schilderij toont de rede van Batavia, het huidige Jakarta in Indonesië, halverwege de zeventiende eeuw. Na het verwoesten van de Javaanse stad Jacatra sticht de Verenigde Oost-Indische Compagnie VOC hier in 1619 een nederzetting, als centrum voor haar Aziatische handelsnetwerk. Vanuit heel Azië worden langs verschillende zeeroutes goederen bijeengebracht, die driemaal per jaar met de retourvloot naar de Republiek worden verscheept."
+    },
+    {
+        "titel":"Zeeschilders met Thijs van Leeuwen",
+        "image":"img/11.jpg",
+        "text":"De text over de aankomende paar kunstwerken is geschreven door Thijs van Leeuwen."
+    },
+    {
+        "titel":"Republiek aan Zee: Michiel de Ruyter",
+        "image":"img/12.jpg",
+        "text":"Dit is een portret van de beroemdste Nederlandse admiraal uit de zeventiende eeuw: Michiel de Ruyter. Met de commandostaf in de hand staat hij afgebeeld als opperbevelhebber van de Nederlandse oorlogsvloot. De rode vlag draagt zijn familiewapen en rechtsonder is zijn vlaggeschip De Zeven Provinciën te zien, hevig verwikkeld in een gevecht op zee."
+    },
+    {
+        "titel":"Willem van de Velde de Jonge met Jeroen van der Vliet",
+        "image":"img/13.jpg",
+        "text":"De tekst over het volgende kunstwerk is geschreven door Jeroen van der Vliet."
+    },
+    {
+        "titel":"Republiek aan Zee: Gezicht op het IJ met s-Lands Zeemagazijn",
+        "image":"img/14.jpg",
+        "text":"Dit schilderij van Reinier Nooms toont een gedeelte van de Amsterdamse haven in 1664. Rechts op het doek is het zeemagazijn van de Admiraliteit van Amsterdam te zien, het huidige Scheepvaartmuseum."
+    },
+    {
+        "titel":"Aan het Engelse Hof met Dominic Seldis",
+        "image":"img/15.jpg",
+        "text":"De tekst over de volgende twee kunstwerken is geschreven door Dominic Seldis."
+    },
+    {
+        "titel":"Republiek aan Zee: Chinese Famille Noire",
+        "image":"img/16.jpg",
+        "text":"Dit porseleinen beeld van een staande Afrikaanse man is in China gemaakt, in opdracht van een Europeaan. In dit beeld komen allerlei Europese en Chinese aannames samen over mensen van een andere cultuur. De rode lippen, opengesperde ogen en de schaarse kleding zijn racistische kenmerken waarmee in Europa en China mensen van Afrikaanse herkomst werden weergegeven. De gouden oorringen en de zogenaamde ‘slavenband’ om zijn hals zijn onmiskenbaar verwijzingen naar de Europese slavenhandel."
+    },
+    {
+        "titel":"Portret Admiraal van Kinsbergen",
+        "image":"img/17.jpg",
+        "text":"Geportretteerde Van Kinsbergen maakt voor het eerst naam als jonge officier tijdens de Slag bij de Doggersbank, een zeeslag tegen Engeland in 1781. Hoewel de slag onbetwist eindigt, wordt hij in Nederland als overwinning gevierd en Van Kinsbergen als held onthaald. Van Kinsbergen maakt een afwisselende loopbaan door die eerst begint in het leger en via de Admiraliteit van Amsterdam leidt tot een baan bij de Russische marine. Terug in de Republiek bepleit hij voor hervorming van de Nederlandse vloot. En als adviseur van de stadhouder weet hij enkele, kleinere, veranderingen door te voeren. Van Kinsbergen wordt later benoemd tot Graaf van Doggersbank en krijgt vele onderscheidingen."
+    },
+    {
+        "titel":"Kalmtes, briesjes, stormen met Laura van der Blij",
+        "image":"img/18.jpg",
+        "text":"De tekst over de volgende paar kunstwerken is geschreven door Laura van der Blij."
+    },
+    {
+        "titel":"Republiek aan Zee: De Secrete Macht",
+        "image":"img/19.jpg",
+        "text":"We zien de haven van Vlissingen in het jaar 1805. Links op de voorgrond staat bevelhebber VerHuell in gezelschap van een aantal marineofficieren. In de haven liggen tientallen kleine schepen. Opvallend detail is dat in de mast van het oorlogsschip dat voor de kerk ligt niet de Nederlandse vlag prijkt maar de Franse tricolore."
+    },
+    {
+        "titel":"Aan de steiger: De Koningssloep",
+        "image":"img/20.jpg",
+        "text":"De Nederlandse Koningssloep werd tussen 1816 en 1818 gebouwd voor Koning Willem I op de Marinewerf in Rotterdam. De Koninklijke ‘Chaloupe’ zoals de sloep officieel heet is een soort gouden koets te water. De sloep past binnen de eeuwenoude traditie waarbij Europese monarchen met een roei- of zeiljacht deelnemen aan feestelijke gelegenheden te water. Dat gold zeker voor een maritieme natie als Nederland."
+    },
+    {
+        "titel":"Aan de steiger: Christiaan Brunings",
+        "image":"img/21.jpg",
+        "text":"Je zou het op het eerste gezicht misschien niet vermoeden maar dit schip de Christiaan Brunings is in het jaar 1900 in Amsterdam gebouwd als ijsbreker. De sterk oplopende boeg heeft aan de voorzijde een diepgang van slechts 20 centimeter, wat helpt bij het breken van het ijs. Het schip beschikt nog altijd over een stoommachine die op steenkool wordt gestookt. Daarmee representeert dit schip ook het tijdperk van de stoomschepen waarvan er maar bijzonder weinig bewaard zijn gebleven."
+    },
+    {
+        "titel":"Teaser Solebay wandtapijten",
+        "image":"img/22.jpg",
+        "text":"-"
     }
 ]
 
-//mytitle.innerHTML = "dit is gevoegd door java";
-//myimage.src = "img/1.jpg";
+//myTitle.innerHTML = "dit is door het script toegevoegd";
+//myImage.src = "img/1.jpg";
 
 function show(index){
-  mytitle.innerHTML = lokaties[index].titel;
-  myimage.src = lokaties[index].image;
+    myTitle.innerHTML = lokaties[index].titel;
+    myImage.src = lokaties[index].image;
+    myText.innerHTML = lokaties[index].text;
 }
-function getinput(){
-  show(myinput.value);
-  console.log(myinput.value);
-  myinput.value = "";
-  myinput.focus();
+
+function getInput(){
+    show(myInput.value);
+    //console.log(myInput.value)
+    myInput.value = "";
+    myInput.focus();
 }
--- meseumonline/tour/tour.js
+++ meseumonline/tour/tour.js
@@ -6,25 +6,37 @@ const myText = document.getElementById("myText");
 let lokaties =
 [
     {
-        "titel":"Nachtwacht",
-        "image":"imgtour/0.jpeg"  
+        "titel":"Ingang",
+        "image":"imgtour/0.JPG",
+        "text":"Met Willem I als nieuwe koning vanaf 1813, verhuisde het museum als 'Rijks Museum' samen met de uit Den Haag afkomstige nationale prentencollectie naar het Trippenhuis, een 17de-eeuws stadspaleis aan de Kloveniersburgwal. Tot ongenoegen van de directeur werden vervolgens allerlei andere collecties elders ondergebracht. Voorwerpen uit de klassieke oudheid gingen naar een nieuw Museum van Oudheden in Leiden en voorwerpen en kunstnijverheid werden aan het inmiddels in Den Haag opgerichte Kabinet van Zeldzaamheden toegewezen. In Haarlem kwam in 1838 in Paviljoen Welgelegen een apart museum voor moderne, 19de-eeuwse kunst. In tegenstelling tot in de Napoleontijd, kreeg het Rijks Museum weinig grote werken."
     },
     {
-        "titel":"Library",
-        "image":"imgtour/1.jpg"
+        "titel":"Nachtwacht",
+        "image":"imgtour/1.jpeg",
+        "text":"Rembrandts beroemdste en grootste schilderij werd gemaakt voor de Kloveniersdoelen. Dit was een van de drie hoofdkwartieren van de Amsterdamse schutterij, de burgerwacht van de stad. Rembrandt was de eerste die op een schuttersstuk alle figuren in actie weergaf. De kapitein, in het zwart, geeft zijn luitenant opdracht dat de compagnie moet gaan marcheren. De schutters stellen zich op. Met behulp van licht vestigde Rembrandt de aandacht op belangrijke details, zoals het handgebaar van de kapitein en het kleine meisje op de voorgrond. Zij is de mascotte van de schutters. De naam Nachtwacht is pas veel later ontstaan, toen men dacht dat het om een nachtelijk tafereel ging."
+    },
+    {
+        "titel":"Bibliotheek",
+        "image":"imgtour/2.jpg",
+        "text":"De Rijksmuseum Research Library is een van de belangrijkste kunstbibliotheken ter wereld. Catalogi van veilingen en tentoonstellingen, handels- en collectiecatalogi, maar ook boeken, tijdschriften en jaarverslagen met betrekking tot de museumcollecties worden sinds 1885 onafgebroken verzameld"
         
     },
     {
-        "titel":"gallery of Honour",
-        "image":"imgtour/2.jpg"
+        "titel":"Gallery of Honour",
+        "image":"imgtour/3.jpg",
+        "text":"De bedreigde zwaan, Jan Asselijn ca. 1650'In dit grootse landschap met de machtige waterval gaan de kleine figuurtjes bijna geheel verloren. De knoestige bomen, de kale takken en het ruisende water beheersen het beeld. Heel het drama van ontstaan en vergaan in de natuur wordt erin voelbaar. Als een baken van rust steekt de kerktoren boven alles uit.",
     },
     {
-        "titel":"Old Ship",
-        "image":"imgtour/3.jpg"
+        "titel":"Willim Rex",
+        "image":"imgtour/4.jpg",
+        "text":"Dit model laat zien hoe een Nederlands oorlogsschip er aan het eind van de 17de eeuw uitzag. Het model is gebouwd op de Vlissingse werf waar ook echte oorlogsschepen van stapel liepen.Deze waren wel ruim twaalf keer zo groot. Dit schip heeft 74 kanonnen aan boord.Het pronkmodel stond opgesteld in de vergaderzaal van de Zeeuwse Admiraliteit in Middelburg."
     },
     {
-        "titel":"The Rubens Gallery",
-        "image":"imgtour/4.jpg"
+        "titel":"Alte Pinakothek",
+        "image":"imgtour/5.jpg",
+        "text":"De energiegerelateerde renovatiewerkzaamheden in de Alte Pinakothek die vierenhalf jaar geleden zijn gestart, zijn inmiddels afgerond. Op 3 juli 2018 zijn alle dertien kamers op de bovenverdieping volledig toegankelijk. Ter gelegenheid van de voltooiing van de renovatie en om deze gelegenheid te vieren, heeft het Rijksmuseum Vermeers Brieflezende vrouw in blauw aan München in bruikleen gegeven",
+        
+
     }  
 ]
 
